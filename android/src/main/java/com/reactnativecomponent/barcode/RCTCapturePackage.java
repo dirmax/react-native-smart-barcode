617a5985a53834ca4bb29ce639e70e71d373b8fb
Александр Хрищанович <dirmax@yandex.ru>
2017-10-31 18:53:29 +0300

    RN v0.49

--- android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
+++ android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
@@ -27,7 +27,6 @@ public class RCTCapturePackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
